2577cb4290cf2ebe65c6fd7c4ad2d955c2fa6c12
TinM16 <cmakilang@southernleytestateu.edu.ph>
2024-10-15 09:05:21 +0800

    This is my code!

---
+++ activity1.py
@@ -0,0 +1,15 @@
+# Integer
+age = 21
+print("Age:", age)
+
+# Float
+height = 5.9
+print("Height:", height)
+
+# String
+name = "John Doe"
+print("Name:", name)
+
+# Boolean
+is_student = False
+print("Is student?", is_student)
